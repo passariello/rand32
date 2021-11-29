3d51f382b39f41dfe7ecea634f9988112814fecb
Dario Passariello <dariopassariello@gmail.com>
2021-11-28 22:11:15 -0800

    changes

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 random 32bit random numbers generator as array.
 
+example:
+
+```js
+console.log( rand32(500000) );
+```
+
 by Dario Passariello - copyright 2021
--- rand32.js
+++ rand32.js
@@ -1,17 +1,27 @@
-  const rand32 = (total) => {
-    const t0 = performance.now()
-    var array = []
-    array['nums'] = []
-    array['time'] = []  
+/*!
+ * rand32 <https://github.com/passariello/rand32>
+ *
+ * Copyright (c) 2021, Dario Passariello.
+ * Licensed under the Apache-2.0 License.
+ */
+
+/***********************************************************************/
+
+const rand32 = (total) => {
     
-    for(var i=0; i < total; ++i){ 
-       array.nums.push( window.crypto.getRandomValues(new Uint32Array(1))[0] )
-    }
-    
-    const t1 = performance.now()
-    array.time.push(t1 - t0)    
-    
-    return array
+  const t0 = performance.now();
+  var array = [];
+  array.nums = [];
+  array.time = [];
+
+  for(var i=0; i < total; ++i){ 
+      array.nums.push( window.crypto.getRandomValues(new Uint32Array(1))[0] );
   }
-  
-  console.log( rand32(500000) )
+
+  const t1 = performance.now();
+  array.time.push(t1 - t0);
+
+  return array;
+};
+
+// console.log( rand32(500000) );
